1a91d412f85b03e8dbc9c839410b91ef2ec5cebc
SrivaniSonti <82490634+SrivaniSonti@users.noreply.github.com>
2021-04-20 13:05:07 +0530

    Final

--- employee-api-data/src/main/java/com/ukable/controller/DepartmentController.java
+++ employee-api-data/src/main/java/com/ukable/controller/DepartmentController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ukable.model.Department;
 import com.ukable.service.DepartmentService;
 
 @RestController
-//@RequestMapping("/get")
+@RequestMapping("/get")
 public class DepartmentController {
 	
 	@Autowired
