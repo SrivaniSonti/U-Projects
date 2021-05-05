3503bdac4c62f2e145bc24c5c09198958297ea00
SrivaniSonti <82490634+SrivaniSonti@users.noreply.github.com>
2021-05-05 22:59:07 +0530

    Added Global Exception Handler

--- employee-api-data/src/main/java/com/ukable/exception/GlobalExceptionHandler.java
+++ employee-api-data/src/main/java/com/ukable/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.ukable.exception;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -12,16 +14,20 @@ import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    /*@ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }*/
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<?> EmptyResultDataAccessException(Exception ex, WebRequest request) {
-       // return new ResponseEntity<>("Data not found with the given ID ", HttpStatus.NOT_FOUND);
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Data not found with the given ID ", HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>("This method is not supported with the given URL ", HttpStatus.NOT_FOUND);
+
     }
 }
